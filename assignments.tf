ca0cfadcca514028e48d4beeef415fd8ddfa3a55
jmarti45 <jmartinez72@hotmail.es>
2024-01-12 11:43:28 +0000

    Update assignments.tf

--- assignments.tf
+++ assignments.tf
@@ -1,12 +1,3 @@
-#/
-# assign reader role for team working on subscription 
-resource "azurerm_role_assignment" "subscription1" {
-  scope = var.subscription
-  #scope                = "/subscriptions/${var.subscription}"
-  role_definition_name = "Contributor"
-  principal_id         = azuread_group.example.object_id
-}
-/#
 # assign reader role for team working on Management group 
 resource "azurerm_role_assignment" "management1" {
   scope                = var.managemetgroup
