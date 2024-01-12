2c829591551e81dfba36d2fb2aec418197f6a7dc
jmarti45 <jmartinez72@hotmail.es>
2024-01-12 15:25:03 +0000

    Update groups.tf

--- groups.tf
+++ groups.tf
@@ -1,7 +1,7 @@
 data "azuread_client_config" "current" {}
 
 resource "azuread_group" "group1" {
-  display_name     = var.security
+  display_name     = var.securitygroup
   owners           = [data.azuread_client_config.current.object_id]
   security_enabled = true
 }
