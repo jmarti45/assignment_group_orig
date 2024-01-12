4ecb5e8f7524aba3fa11b34b3874d8fe20813a37
jmarti45 <jmartinez72@hotmail.es>
2024-01-12 11:44:53 +0000

    Update assignments.tf

--- assignments.tf
+++ assignments.tf
@@ -1,6 +1,6 @@
 # assign reader role for team working on Management group 
 resource "azurerm_role_assignment" "management1" {
-  scope                = var.managemetgroup
+  scope                = var.managementgroup
   role_definition_name = "reader"
   principal_id         = azuread_group.example.object_id
 }
