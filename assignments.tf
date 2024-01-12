d92c99a3ba16d45d58a11471ed3b2fc49ad39933
jmarti45 <jmartinez72@hotmail.es>
2024-01-12 11:46:48 +0000

    Update assignments.tf

--- assignments.tf
+++ assignments.tf
@@ -2,6 +2,6 @@
 resource "azurerm_role_assignment" "management1" {
   scope                = var.managementgroup
   role_definition_name = "reader"
-  principal_id         = azuread_group.example.object_id
+  principal_id         = azuread_group.group1.object_id
 }
 
